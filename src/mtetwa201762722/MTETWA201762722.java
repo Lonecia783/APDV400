818d4d5025ef7cb5b1f3f3d5407c92cdacfd9e6e
201762722 <201762722@MBO_ICT102_09-PC.ump.ac.za>
2020-07-17 13:21:03 +0200

    First commit

---
+++ src/mtetwa201762722/MTETWA201762722.java
@@ -0,0 +1,21 @@
+
+package mtetwa201762722;
+
+/**
+ *
+ * @author 201762722
+ */
+public class MTETWA201762722 {
+
+  
+    public static void main(String[] args) {
+       
+        int Q = 5;
+        int R = 16;
+        int SUM = Q + R;
+        
+        System.out.println("The sum is: " + SUM);
+        
+    }
+    
+}
